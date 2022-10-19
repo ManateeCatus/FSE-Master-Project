d6ef43490a91efb7529a747576128cbfc364a79a
ddombro3 <115665642+ddombro3@users.noreply.github.com>
2022-10-19 14:05:28 -0700

    Add files via upload

---
+++ Excercise3/jsFunc/sketch.js
@@ -0,0 +1,12 @@
+const points = [];
+let dragPoint = null;
+
+const numPoints = 5;
+const dragRadius = 20;
+
+function setup() {
+createCanavs(600, 600);
+  strokeWeight(5);
+  
+  for(let i = 0; i < numPoints; i++)
+}
---
+++ Excercise3/jsShapes/sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background(220);
+  fill('blue')
+  square(280,250,70)
+  fill('blue')
+  square(40,200,70)
+  line(280,290,100,240)
+  square(280,250,70)
+  fill('red')
+  circle(70,80,70)
+  fill('red')
+  circle(240,360,70)
+  line(70,80,240,360)
+  fill('yellow')
+  square(300,90,70)
+  fill('yellow')
+  square(90,300,70)
+  line(320,130,120,320)
+ 
+}
+
+function moveShapes() {
+}
